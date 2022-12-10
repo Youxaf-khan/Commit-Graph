2c0e857c641f57c2204500020d6b889d91279c51
Youxaf-khan <youxafkhan@htomail.com>
2022-12-10 17:58:04 +0500

    2022-12-10T17:58:04+05:00

--- index.js
+++ index.js
@@ -5,70 +5,70 @@ const random = require('random');
 
 const FILE_PATH = './data.json';
 
-const makeCommit = n => {
-  if(n===0) return simpleGit.push();
+// const makeCommit = n => {
+//   if(n===0) return simpleGit.push();
   
-  // Generate a random date between 1 year ago and today
-  let DATE = moment().subtract(1, 'y').add(1, 'd').format();
-  let dayOfWeek = moment(DATE).day(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
+//   // Generate a random date between 1 year ago and today
+//   let DATE = moment().subtract(1, 'y').add(1, 'd').format();
+//   let dayOfWeek = moment(DATE).day(); // 0 = Sunday, 1 = Monday, ..., 6 = Saturday
   
-  // Generate a new date until we get a working day
-  while (dayOfWeek === 0 || dayOfWeek === 6) {
-    // Add 1 day to DATE and update dayOfWeek
-    DATE = moment(DATE).add(1, 'd').format();
-    dayOfWeek = moment(DATE).day();
-  }
+//   // Generate a new date until we get a working day
+//   while (dayOfWeek === 0 || dayOfWeek === 6) {
+//     // Add 1 day to DATE and update dayOfWeek
+//     DATE = moment(DATE).add(1, 'd').format();
+//     dayOfWeek = moment(DATE).day();
+//   }
   
-  // Reduce the probability of making commits on weekends
-  if (dayOfWeek === 5) { // Friday
-    if (random.int(0, 3) === 0) { // 25% probability
-      // Add 3 days to DATE to simulate a long weekend
-      DATE = moment(DATE).add(3, 'd').format();
-    }
-  } else if (dayOfWeek === 6) { // Saturday
-    if (random.int(0, 1) === 0) { // 50% probability
-      // Add 2 days to DATE to simulate a weekend
-      DATE = moment(DATE).add(2, 'd').format();
-    }
-  }
+//   // Reduce the probability of making commits on weekends
+//   if (dayOfWeek === 5) { // Friday
+//     if (random.int(0, 3) === 0) { // 25% probability
+//       // Add 3 days to DATE to simulate a long weekend
+//       DATE = moment(DATE).add(3, 'd').format();
+//     }
+//   } else if (dayOfWeek === 6) { // Saturday
+//     if (random.int(0, 1) === 0) { // 50% probability
+//       // Add 2 days to DATE to simulate a weekend
+//       DATE = moment(DATE).add(2, 'd').format();
+//     }
+//   }
 
-  const data = {
-    date: DATE
-  }
+//   const data = {
+//     date: DATE
+//   }
   
-  console.log(DATE);
-  jsonfile.writeFile(FILE_PATH, data, ()=>{
-    simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }, makeCommit.bind(this, --n));
-  });
-}
-makeCommit(100);
+//   console.log(DATE);
+//   jsonfile.writeFile(FILE_PATH, data, ()=>{
+//     simpleGit().add([FILE_PATH]).commit(DATE, {'--date': DATE }, makeCommit.bind(this, --n));
+//   });
+// }
+// makeCommit(100);
 
 // From the video 
 
-// const makeCommit = (n) => {
-//   if (n === 0) return simpleGit.push()
+const makeCommit = (n) => {
+  if (n === 0) return simpleGit.push()
 
-//   const x = random.int(0, 50)
-//   const y = random.int(0, 5)
-//   const DATE = moment()
-//     .subtract(1, "y")
-//     .add(1, "d")
-//     .add(x, "w")
-//     .add(y, "d")
-//     .format()
+  const x = random.int(0, 50)
+  const y = random.int(0, 5)
+  const DATE = moment()
+    .subtract(1, "y")
+    .add(1, "d")
+    .add(x, "w")
+    .add(y, "d")
+    .format()
 
-//   const data = {
-//     date: DATE,
-//   }
+  const data = {
+    date: DATE,
+  }
 
-//   console.log(DATE)
-//   jsonfile.writeFile(FILE_PATH, data, () => {
-//     simpleGit()
-//       .add([FILE_PATH])
-//       .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n))
-//   })
-// }
-// makeCommit(300)
+  console.log(DATE)
+  jsonfile.writeFile(FILE_PATH, data, () => {
+    simpleGit()
+      .add([FILE_PATH])
+      .commit(DATE, { "--date": DATE }, makeCommit.bind(this, --n))
+  })
+}
+makeCommit(300)
 
 
 // Another solution -------------------------------
